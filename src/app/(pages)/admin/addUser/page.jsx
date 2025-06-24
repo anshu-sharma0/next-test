9d2a159e1a24c1d123d182fdc3d85c51d0024fb9
Anshu <ashu.dnn@gmail.com>
2025-06-24 12:37:19 +0530

    feat/folder-structure

--- src/app/(pages)/admin/addUser/page.jsx
+++ src/app/(pages)/admin/addUser/page.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { toast } from 'react-hot-toast';
-import { AddUser } from '../../../../components/layout';
+import AddUser from './addUser'
 import { addUser } from '../../../../services/userService';
 
 const Page = () => {
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,8 @@
 import Pagination from "./pagination"
 import FilterLog from './RoleFilter'
 import Loader from './Loader'
-import LoginForm from './loginForm'
-import AddUser from './addUser'
+import LoginForm from './auth/loginForm'
 import LogTable from './tables/logTable'
 import UserTable from './tables/userTable'
 
-export { Pagination, FilterLog, LogTable, Loader, LoginForm, UserTable, AddUser }
+export { Pagination, FilterLog, LogTable, Loader, LoginForm, UserTable }
